b34878b559159b85f60bce7eabfee2ed0c738b21
Tanaka <tanakakenne2000@gmail.com>
2023-05-13 22:43:38 +0200

    initial

---
+++ src/main/java/com/mentor/me/backend/MentorMeBackendApplication.java
@@ -0,0 +1,13 @@
+package com.mentor.me.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MentorMeBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MentorMeBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mentor/me/backend/MentorMeBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mentor.me.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MentorMeBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
